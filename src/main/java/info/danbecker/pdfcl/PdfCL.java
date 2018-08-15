6464d4904d1f6fea7f1967d083617557e5f79ac7
beckerdo <dan@danbecker.info>
2018-08-15 09:24:07 -0500

    Add ability to work with multiple source inputs

--- src/main/java/info/danbecker/pdfcl/PdfCL.java
+++ src/main/java/info/danbecker/pdfcl/PdfCL.java
@@ -32,44 +32,44 @@ import com.itextpdf.layout.property.AreaBreakType;
 public class PdfCL {
     /** LOGGER */
     public static final Logger LOGGER = LoggerFactory.getLogger(PdfCL.class);
-    
-    public static final String SRC = "resources/input.pdf";
-    public static final String SRC2 = "resources/input2.pdf";
+
+    public static final String[] SRC = { "resources/input.pdf" };
     public static final String DEST = "resources/output.pdf";
-    
+
+    public static final String CMD_DELIM = "\"\\\\s*,\\\\s*\""; // 0* whitespace, comma, 0* whtitespace
+
     protected static String verb;
-    protected static String src;
-    protected static String src2;
+    protected static String[] src;
     protected static String dest;
     protected static int number;
     protected static List<Integer> list;
-    
+
     // Constructors
     // Runtime
     public static void main(String[] args) throws Exception {
         // Parse command line options
         parseOptions(args);
-        
-        if ( null != verb && verb.length() > 0) {
-            switch ( verb ) {
-                case "create": {
-                    new PdfCL().createPdf( dest, number );
-                    break;
-                }
-                case "append": {
-                    new PdfCL().appendPdf( src, dest, list );
-                    break;
-                }
-                case "reverse": {
-                    new PdfCL().reversePdf( dest );
-                    break;
-                }
-                default: {
-                    LOGGER.info("verb \"" + verb + "\" is unknown");                   
-                }       
+
+        if (null != verb && verb.length() > 0) {
+            switch (verb) {
+            case "create": {
+                new PdfCL().createPdf(dest, number);
+                break;
+            }
+            case "append": {
+                new PdfCL().appendPdf(src, dest, list);
+                break;
+            }
+            case "reverse": {
+                new PdfCL().reversePdf(src);
+                break;
+            }
+            default: {
+                LOGGER.info("verb \"" + verb + "\" is unknown");
+            }
             }
         }
-        LOGGER.info( "exiting" );
+        LOGGER.info("exiting");
     }
 
     /** Command line options for this application. */
@@ -80,15 +80,15 @@ public class PdfCL {
         options.addOption("h", "help", false, "print the command line options");
         options.addOption("v", "verb", true, "action to perform");
         options.addOption("n", "number", true, "number, such as number of pages");
-        options.addOption("l", "list", true, "list, such as comma separated list of pages");
-        options.addOption("s", "src", true, "input PDF file");
-        options.addOption("s2", "src2", true, "input PDF file");
+        options.addOption("l", "list", true, "list of comma-separated, such as pages, names, etc.");
+        options.addOption("s", "src", true, "list of comma-separated input PDF files");
         options.addOption("d", "dest", true, "output PDF file");
 
         final CommandLineParser cliParser = new DefaultParser();
         final CommandLine line = cliParser.parse(options, args);
-        // line.getArgList(); // Retrieve any left-over non-recognized options and arguments
-        
+        // line.getArgList(); // Retrieve any left-over non-recognized options and
+        // arguments
+
         LOGGER.info("hasHelp=" + line.hasOption("help"));
         // Gather command line arguments for execution
         if (line.hasOption("help")) {
@@ -101,16 +101,11 @@ public class PdfCL {
             LOGGER.info("verb=" + verb);
         }
         if (line.hasOption("src")) {
-            src = line.getOptionValue("src");
+            String option = line.getOptionValue("src");
+            src = option.split(CMD_DELIM);
             LOGGER.info("src=" + src);
         } else {
             src = SRC;
-        }
-        if (line.hasOption("src2")) {
-            src2 = line.getOptionValue("src2");
-            LOGGER.info("src2=" + src2);
-        } else {
-            src2 = SRC2;
         }
         if (line.hasOption("dest")) {
             dest = line.getOptionValue("dest");
@@ -126,131 +121,152 @@ public class PdfCL {
         }
         if (line.hasOption("list")) {
             String stringList = line.getOptionValue("list");
-            List<String> items = Arrays.asList(stringList.split("\\s*,\\s*"));
+            List<String> items = Arrays.asList(stringList.split(CMD_DELIM));
             list = new LinkedList<Integer>();
-            for ( String item: items ) {
-                list.add(Integer.parseInt( item ));                
+            for (String item : items) {
+                list.add(Integer.parseInt(item));
             }
             LOGGER.info("list=" + list);
         } else {
             list = Arrays.asList();
         }
     }
-    
-    /** 
+
+    /** Make/create directory structure for given file name or path. */
+    public static boolean mkdirs(String dest) {
+        return new File(dest).getParentFile().mkdirs();
+    }
+
+    /** States if file exists, is readable, and is non-zero length. */
+    public static boolean fileReadable(String dest) {
+        File src = new File(dest);
+        if (src.exists() && src.canRead() && src.length() > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
      * Creates a dummy PDF
-     * @param dest output file name
-     * @param numPages number of pages
+     * 
+     * @param dest
+     *            output file name
+     * @param numPages
+     *            number of pages
      * @throws IOException
      */
     public void createPdf(String dest, int numPages) throws IOException {
-        File file = new File(dest);
-        file.getParentFile().mkdirs();
-        
+        mkdirs(dest);
+
         // Initialize PDF writer
         PdfWriter writer = new PdfWriter(dest);
- 
+
         // Initialize PDF document
         PdfDocument pdf = new PdfDocument(writer);
- 
+
         // Initialize document
         Document document = new Document(pdf);
- 
+
         // Add pages to the document
         AreaBreak areaBreak = new AreaBreak(AreaBreakType.NEXT_PAGE);
-        if ( numPages == 0 ) {
-            document.add(new Paragraph("")); // documents cannot be blank when closed            
+        if (numPages == 0) {
+            document.add(new Paragraph("")); // documents cannot be blank when closed
         } else {
-            for ( int i = 0; i < numPages; i++ ) {
-                document.add(new Paragraph( "Page" + Integer.toString(i + 1)));
-                if ( i + 1 < numPages) {
+            for (int i = 0; i < numPages; i++) {
+                document.add(new Paragraph("Page" + Integer.toString(i + 1)));
+                if (i + 1 < numPages) {
                     document.add(areaBreak);
                 }
             }
         }
- 
-        //Close document
+
+        // Close document
         document.close();
-        LOGGER.info( "\"" + dest + "\" created");
+        LOGGER.info("\"" + dest + "\" created");
     }
-    
-    public void appendPdf(String src, String dest, List<Integer> pagesToMerge) throws IOException {
-        File file = new File(dest);
-        file.getParentFile().mkdirs();       
-        
+
+    public void appendPdf(String[] srcs, String dest, List<Integer> pagesToMerge) throws IOException {
+        mkdirs(dest);
         PdfDocument pdfDest = new PdfDocument(new PdfWriter(dest));
-        PdfDocument pdfSrc = new PdfDocument(new PdfReader(src));
 
-        pdfSrc.copyPagesTo( pagesToMerge, pdfDest);
-        
-        pdfSrc.close();
+        for (String src : srcs) {
+            PdfDocument pdfSrc = new PdfDocument(new PdfReader(src));
+            pdfSrc.copyPagesTo(pagesToMerge, pdfDest);
+            pdfSrc.close();
+        }
+
         pdfDest.close();
     }
 
-//    public void appendPdf(String src, String dest, List<Integer> pagesToMerge) throws IOException {
-//        File file = new File(dest);
-//        file.getParentFile().mkdirs();
-//        
-//        
-//        PdfDocument pdfOriginalPages = null;
-//        if (file.exists() && file.length() > 0) {
-//            pdfOriginalPages = new PdfDocument(new PdfReader(dest));
-//        }
-//        PdfDocument pdfDest = new PdfDocument(new PdfWriter(dest));
-//        // PdfDocument pdfDest = new PdfDocument(new PdfReader(dest), new PdfWriter(dest));
-//        PdfDocument pdfSrc = new PdfDocument(new PdfReader(src));
-//
-//        PdfMerger merger = new PdfMerger(pdfDest); // cannot append to existing doc
-//        if (file.exists() && file.length() > 0) {
-//            merger.merge(pdfDest, 1, pdfOriginalPages.getNumberOfPages());
-//        }
-//        merger.merge(pdfSrc, pagesToMerge);
-//        // pdfSrc.copyPagesTo( pagesToMerge, pdfDest);
-//        
-//        if (file.exists() && file.length() > 0) {
-//            pdfOriginalPages.close();
-//        }
-//        pdfSrc.close();
-//        pdfDest.close();
-//    }
+    // public void appendPdf(String src, String dest, List<Integer> pagesToMerge)
+    // throws IOException {
+    // File file = new File(dest);
+    // file.getParentFile().mkdirs();
+    //
+    //
+    // PdfDocument pdfOriginalPages = null;
+    // if (file.exists() && file.length() > 0) {
+    // pdfOriginalPages = new PdfDocument(new PdfReader(dest));
+    // }
+    // PdfDocument pdfDest = new PdfDocument(new PdfWriter(dest));
+    // // PdfDocument pdfDest = new PdfDocument(new PdfReader(dest), new
+    // PdfWriter(dest));
+    // PdfDocument pdfSrc = new PdfDocument(new PdfReader(src));
+    //
+    // PdfMerger merger = new PdfMerger(pdfDest); // cannot append to existing doc
+    // if (file.exists() && file.length() > 0) {
+    // merger.merge(pdfDest, 1, pdfOriginalPages.getNumberOfPages());
+    // }
+    // merger.merge(pdfSrc, pagesToMerge);
+    // // pdfSrc.copyPagesTo( pagesToMerge, pdfDest);
+    //
+    // if (file.exists() && file.length() > 0) {
+    // pdfOriginalPages.close();
+    // }
+    // pdfSrc.close();
+    // pdfDest.close();
+    // }
 
-    protected void reversePdf(String dest) throws Exception {
-        File src = new File( dest );
-        if ( !src.exists() || src.length() < 1) {
-            LOGGER.info( "File \"" + dest + "\" exists=" + src.exists() + ", length=" + src.length());
-            return;
+    protected void reversePdf(String[] srcs) throws Exception {
+        for (String src : srcs) {
+            File srcFile = new File(src);
+            if (fileReadable(dest)) {
+                LOGGER.info("File \"" + srcFile + "\" exists=" + srcFile.exists() + ", canRead=" + srcFile.canRead() + ", length="
+                        + srcFile.length());
+                return;
+            }
+            LOGGER.info("Source file=" + src);
+            byte[] byteArray = Files.readAllBytes(srcFile.toPath());
+            PdfDocument srcDoc = new PdfDocument(
+                    new PdfReader(new RandomAccessSourceFactory().createSource(byteArray), new ReaderProperties()));
+            PdfDocument resultDoc = new PdfDocument(new PdfWriter(dest));
+            resultDoc.initializeOutlines();
+
+            List<Integer> pages = new ArrayList<>();
+            int numPages = srcDoc.getNumberOfPages();
+            LOGGER.info("NumPages=" + numPages);
+            for (int pagei = numPages; pagei > 0; pagei--) {
+                pages.add(pagei);
+            }
+            LOGGER.info("Pages=" + pages);
+            srcDoc.copyPagesTo(pages, resultDoc);
+
+            resultDoc.close();
+            srcDoc.close();
         }
-        byte[] byteArray = Files.readAllBytes(src.toPath());
-        PdfDocument srcDoc = new PdfDocument(
-           new PdfReader(new RandomAccessSourceFactory().createSource(byteArray),
-           new ReaderProperties()));
-        PdfDocument resultDoc = new PdfDocument(new PdfWriter(dest));
-        resultDoc.initializeOutlines();
- 
-        List<Integer> pages = new ArrayList<>();
-        int numPages = srcDoc.getNumberOfPages();
-        LOGGER.info( "NumPages=" + numPages );
-        for ( int pagei = numPages; pagei > 0; pagei-- ) {
-            pages.add( pagei );
-        }
-        LOGGER.info( "Pages=" + pages );
-        srcDoc.copyPagesTo(pages, resultDoc);
- 
-        resultDoc.close();
-        srcDoc.close();
     }
-    
-    public void concatenatePdf(String src1, String src2, String dest) throws IOException {
-        PdfDocument pdfDest= new PdfDocument(new PdfWriter(dest));
-        PdfDocument pdfSrc1 = new PdfDocument(new PdfReader(src1));
-        PdfDocument pdfSrc2 = new PdfDocument(new PdfReader(src2));
-        
+
+    public void concatenatePdf(String[] srcs, String dest) throws IOException {
+        PdfDocument pdfDest = new PdfDocument(new PdfWriter(dest));
+
         PdfMerger merger = new PdfMerger(pdfDest);
-        merger.merge(pdfSrc1, 1, pdfSrc1.getNumberOfPages());
-        merger.merge(pdfSrc2, 1, pdfSrc2.getNumberOfPages());
-        
+        for (String src : srcs) {
+            LOGGER.info("Source file=" + src);
+            PdfDocument pdfSrc = new PdfDocument(new PdfReader(src));
+            merger.merge(pdfSrc, 1, pdfSrc.getNumberOfPages());
+            pdfSrc.close();
+        }
+
         pdfDest.close();
-        pdfSrc1.close();
-        pdfSrc2.close();
     }
 }
